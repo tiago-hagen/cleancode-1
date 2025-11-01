b326dc245b956617d4e0f58cd01462682f4f0c4d
tiago-hagen <141409370+tiago-hagen@users.noreply.github.com>
2025-11-01 09:19:19 -0300

    Update import statement for Stud model
    test2

--- java/app/src/test/java/studentdb/database/StudDatabaseTest.java
+++ java/app/src/test/java/studentdb/database/StudDatabaseTest.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import studentdb.model.Stud;
+
 
 /**
  * Test suite for StudentDatabase interface.
